0b9e4baa90e38eea3fea42086a406094dd181350
moromis <moromise@gmail.com>
2016-12-01 12:20:12 -0800

    Adding cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/SudokuGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SudokuGenerator.dir/Driver.cpp.o"
+  "CMakeFiles/SudokuGenerator.dir/Board.cpp.o"
+  "CMakeFiles/SudokuGenerator.dir/Square.cpp.o"
+  "SudokuGenerator.pdb"
+  "SudokuGenerator.exe"
+  "libSudokuGenerator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SudokuGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
